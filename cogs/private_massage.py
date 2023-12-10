9a633fa19735d6ed563a9271fa9d5a205f1c1c95
Enisey23 <106614504+Enisey23@users.noreply.github.com>
2023-12-10 14:23:19 +0300

    Add listener

--- cogs/private_massage.py
+++ cogs/private_massage.py
@@ -6,6 +6,10 @@ class Private_massage(commands.Cog):
     self.bot = bot
 
   @commands.Cog.listener()
+  async def on_ready(self):
+    print(f'Bot Loaded | private_massage.py ✅')
+
+  @commands.Cog.listener()
   async def on_guild_join(self, guild):
         owner = await self.bot.fetch_user(guild.owner_id)
         await owner.send(f"Привет, я бот **{self.bot.user.name}**! Спасибо, что добавили меня на свой сервер!\n"
