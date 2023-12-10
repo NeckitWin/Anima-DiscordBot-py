385f79422830547e9b33db95a5044bd7a19be9dd
Enisey23 <106614504+Enisey23@users.noreply.github.com>
2023-12-10 14:18:27 +0300

    Add files via upload
    Add private_massage

---
+++ cogs/private_massage.py
@@ -0,0 +1,16 @@
+import disnake
+from disnake.ext import commands
+
+class Private_massage(commands.Cog):
+  def __init__(self, bot):
+    self.bot = bot
+
+  @commands.Cog.listener()
+  async def on_guild_join(self, guild):
+        owner = await self.bot.fetch_user(guild.owner_id)
+        await owner.send(f"Привет, я бот **{self.bot.user.name}**! Спасибо, что добавили меня на свой сервер!\n"
+                         f"Мой префикс - `!`\n"
+                         f"Чтобы узнать список моих команд, напишите `.help`")
+
+def setup(bot):
+    bot.add_cog(Private_massage(bot))
